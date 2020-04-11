c869097379da806af29e061401e8f8c0d83d4103
JCGALVIS <jcgalvis6@hotmail.com>
2020-04-10 21:30:47 -0500

    Agregar función que lee el presupuesto que el usuario ingresa.

--- src/componentes/Pregunta.js
+++ src/componentes/Pregunta.js
@@ -1,14 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+
+    //Definir el state
+    const [cantidad, guardarCatindad] = useState(0);
+
+    //Función que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCatindad(parseInt(e.target.value, 10));
+    };
+
+    //Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        //Validar
+
+
+        //Si se pasa la validación
+    }
+
     return (  
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
-            <form>
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
                 <input 
                     type="submit"
