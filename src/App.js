53d36314bd1e4837544d83a69ed37c3f27d79063
JCGALVIS <jcgalvis6@hotmail.com>
2020-04-13 14:57:20 -0500

    Creando componente formulario.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Pregunta from './componentes/Pregunta'
+import Formulario from './componentes/Formulario'
 
 function App() {
+  //Definir el statue
+  const [presupuesto, guardarPresupuesto] = useState(0);
+  const [restante, guardarRestante] = useState(0);
+
   return (
     <div className="container">
       <header>
         <h1>Gatos semanales</h1>
 
-        <div className="contemido-principal contenido">
-          <Pregunta/>
+        <div className="contenido-principal contenido">
+          <Pregunta
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
+          />
+
+          <div className="row">
+            <div className="one-half column">
+              <Formulario/>
+            </div>
+            <div className="one-half column">
+              2
+            </div>
+          </div>
         </div>
       </header>
     </div>
