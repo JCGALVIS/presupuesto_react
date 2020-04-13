48fa206352401de61598de8a5c79233ade7a374b
JCGALVIS <jcgalvis6@hotmail.com>
2020-04-13 15:04:50 -0500

    Condicionano componentes de Formulario con preguntas.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   //Definir el statue
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
+  const [mostrarpregunta, actualizarPregunta] = useState(true);
 
   return (
     <div className="container">
@@ -13,19 +14,29 @@ function App() {
         <h1>Gatos semanales</h1>
 
         <div className="contenido-principal contenido">
-          <Pregunta
-            guardarPresupuesto={guardarPresupuesto}
-            guardarRestante={guardarRestante}
-          />
+          {
+            mostrarpregunta
+            ? (
+                <Pregunta
+                  guardarPresupuesto={guardarPresupuesto}
+                  guardarRestante={guardarRestante}
+                  actualizarPregunta={actualizarPregunta}
+                />
+              )
+            : (
+                <div className="row">
+                  <div className="one-half column">
+                    <Formulario/>
+                  </div>
+                  <div className="one-half column">
+                    2
+                  </div>
+                </div>
+              )
+          }
+          
 
-          <div className="row">
-            <div className="one-half column">
-              <Formulario/>
-            </div>
-            <div className="one-half column">
-              2
-            </div>
-          </div>
+          
         </div>
       </header>
     </div>
