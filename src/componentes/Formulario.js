748eaca9c9981e8791d8a0979a00974c489c4fb2
JCGALVIS <jcgalvis6@hotmail.com>
2020-04-13 17:00:48 -0500

    Restar presupuesto al añadir un gasto.

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Error from './Error';
 import shortid from 'shortid';
 
-const Formulario = ({agregarNuevoGasto}) => {
+const Formulario = ({guardarGasto, guardarCrearGasto}) => {
     //Definir state
     const [nombre, guardarNombre] = useState('');
     const [cantidad, guardarCantidad] = useState(0);
@@ -27,7 +27,8 @@ const Formulario = ({agregarNuevoGasto}) => {
         }
 
         //Pasar el gasto al componente principal
-        agregarNuevoGasto(gasto);
+        guardarGasto(gasto);
+        guardarCrearGasto(true);
         
         //Resetear el form
         guardarNombre('');
